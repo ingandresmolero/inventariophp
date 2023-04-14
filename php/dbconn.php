59c63dd54d076c0f5867a13aa9169dfc99997ce4
ingandresmolero <87983934+ingandresmolero@users.noreply.github.com>
2023-04-14 12:01:16 -0400

    Paginacion en Proceso

--- php/dbconn.php
+++ php/dbconn.php
@@ -1,12 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<?php
+try{
+    $user = "root";
+    $passw = "";
+    $conn=new PDO("mysql:host=localhost;dbname=gestionsys",$user,$passw);
+
+}catch(PDOException $e){
+    print "Error!: " . $e->getMessage()."<br/>";
+    die();
+}
     
-</body>
-</html>
+
+?>
