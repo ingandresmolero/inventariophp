6ad67bf678dd3392cf08e1781be4b099469603a7
ingandresmolero <87983934+ingandresmolero@users.noreply.github.com>
2023-04-17 10:27:20 -0400

    ultimos cambios hechos de validacion

--- php/login.php
+++ php/login.php
@@ -1,12 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<?php
+  
+
+  include("dbconn.php");
+  session_start();
+  if(isset($_POST["login"])){
     
-</body>
-</html>
+    if($_POST["usuario"]== "" or $_POST["passw"]==""){
+        echo "<center><h1>Usuario y contrasena no deben estar vacios</h1></center>";
+
+}else{
+    $username =$_POST["usuario"];
+    $passw=$_POST["passw"];
+    $query=$conn->prepare("SELECT * FROM usuarios WHERE user='$username' and passw='$passw'");
+    $query -> execute();
+    $control=$query ->fetchAll();
+    if($control>0){
+        $_SESSION["username"]=$username;
+      
+        header("Location:views/dashboard.php");
+    }
+    echo "<center><h1>Algun dato incorrecto.</h1></center>";
+  }
+
+}
+?>
