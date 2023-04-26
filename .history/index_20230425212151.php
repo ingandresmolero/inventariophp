e3d99e910f07842bcc45e6b3b44ce7ef6524695c
M.C <71975116+Liryo@users.noreply.github.com>
2023-04-25 21:53:47 -0400

    Comit 5
    A Green Background for all views!

--- .history/index_20230425212151.php
+++ .history/index_20230425212151.php
@@ -1,20 +1,20 @@
 <?php include('php/login.php'); ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/styles.min.css">
-    <title>Login</title>
+    <title>System</title>
 </head>
 
-<body class="login-page">
+<body>
+   
 
-    <section class="container">
-        <div class="box">
+    <section class="login-page">
+        <div class="container">
             <p>Iniciar Sesión</p>
             <form action="" method="post">
                 
--- .history/index_20230425214205.php
+++ .history/index_20230425214205.php
@@ -1,6 +1,6 @@
 <?php include('php/login.php'); ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="UTF-8">
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/styles.min.css">
-    <title>Login</title>
+    <title>System</title>
 </head>
 
 <body class="login-page">
