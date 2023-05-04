c54a16958c2dec3b8a7e7a9129c966de186ab5d0
ingandresmolero <87983934+ingandresmolero@users.noreply.github.com>
2023-05-04 15:47:37 -0400

    se agrego favicon

--- views/usuarios.php
+++ views/usuarios.php
@@ -30,6 +30,8 @@ $paginas = ceil($total_usuario / $usuarios_x_pagina);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/styles.min.css">
     <title>Usuarios</title>
+    <link rel="icon" type="image/x-icon" href="../img/favicon.png">
+
 </head>
 
 <body>
