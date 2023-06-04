daa6fc9a22eee6ffd7e9e4f9c9a2c83b9fc03f2e
M.C <71975116+Liryo@users.noreply.github.com>
2023-06-03 20:34:02 -0400

    Formulas

--- js/productFuction.js
+++ js/productFuction.js
@@ -2,16 +2,14 @@
             //Editar Stock View
 //------------------------------------------
 
-
 //Formula para calcular el costo promedio.
 
-let costoP = document.getElementById('costoP_input');
-let cantidad = document.getElementById('cantidad_input');
-let ingreso = document.getElementById('ingreso_input')
-let montocompra = document.getElementById('costoPAnt_input');
-let precPvp = document.getElementById('precioPvp_input'); 
-let utilidad = document.getElementById('utilidad_input');
-let precio1 = document.getElementById('precio1_input');
+let costoP = document.getElementById('costoP_input');//Costo Promedio $:
+let cantidad = document.getElementById('cantidad_input'); //existencia
+let ingreso = document.getElementById('ingreso_input'); //Existencia Compra:
+let montocompra = document.getElementById('costoPAnt_input'); //Costo Promedio Anterior $:
+let precPvp = document.getElementById('precioPvp_input');  //pvp
+
 let precio2 = document.getElementById('precio2_input');
 let precio3 = document.getElementById('precio3_input');
 
@@ -21,22 +19,17 @@ function costoPromedio() {
     let cantidadStock = Number(cantidad.value);
     let ingresos = Number(ingreso.value);
     let montCompra = Number(montocompra.value);
-    // let precio1 = Number(precio1_input.value);
-    let precio2 = Number(precio2_input.value);
-    let precio3 = Number(precio3_input.value);
-    let utilidad = Number(utilidad.value);
-
+    
     //Calculo de Costo Promedio
     let costoProm = ( (cantidadStock * costoPromedio) + (ingresos * montCompra) ) / (cantidadStock + ingresos) ;
     precPvp.value = costoProm;
 
-
     //Calculo de Utilidad en base al Precio
-    let utilidadCal = 100 - ((costoPromedio * 100) / precio3);
-    precio2.value=utilidadCal; //esto lo puedes mostrar en precio 2
+    let utilidadCal = 100 - ((costoPromedio * 100) / precPvp.value);
+    precio2.value=utilidadCal;
 
     //calculo de precio en base a la utilidad
-    let precioutilidad = costoPromedio / ((100-(utilidad/100)));
+    let precioutilidad = costoPromedio / ((100-(utilidadCal/100)));
     precio3.value = precioutilidad;
   }
   
@@ -44,16 +37,5 @@ function costoPromedio() {
   cantidad.addEventListener('input', costoPromedio);
   ingreso.addEventListener('input', costoPromedio);
   montocompra.addEventListener('input', costoPromedio);
-
+  precPvp.addEventListener('input', costoPromedio);
 //---------------------------------------------- End
-
-
-
-
-
-//Formula 2:
-
-
-
-
-// Formula 3:
